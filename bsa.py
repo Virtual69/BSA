bb37f8f7beab433af9c5ed2168f685f448d2a088
Virtual69 <76128820+Virtual69@users.noreply.github.com>
2020-12-19 19:44:58 +0530

    Update bsa.py

--- bsa.py
+++ bsa.py
@@ -1,8 +1,8 @@
 
 
 first_number =  input('first name: ')
-secound_number = input('secound name: ')
+secound_number = input('second name: ')
 
 bsa = first_number + secound_number
 
-print(f'welcome to our group  {bsa} ')
+print(f'welcome to our B.S.A  {bsa} ')
